0097e1e90e379e96aad20bafd907fa9e5e30fd7b
Adrian Ulbrich <adrian.ulbrich.2003@gmail.com>
2026-07-25 18:36:53 +0200

    Emit libc and utils artifacts to root build/ when building from the root project

--- utils/Include.mk
+++ utils/Include.mk
@@ -10,13 +10,19 @@ ifndef PROGRESS_LABEL
 PROGRESS_LABEL = echo
 endif
 
+ifeq ($(MAKE_ROOT),$(UTILS_ROOT))
+UTILS_BUILD_DIR := $(MAKE_ROOT)/build
+else
+UTILS_BUILD_DIR := $(MAKE_ROOT)/build/libc
+endif
+
 UTILS := cat clear ls mkdir mount pwd rename rm rmdir setsid sleep stty umount xxd ysh
-UTILS_BINS = $(patsubst %, $(UTILS_ROOT)/build/%, $(UTILS))
+UTILS_BINS = $(patsubst %, $(UTILS_BUILD_DIR)/%, $(UTILS))
 
 UTILS_CFLAGS := $(CFLAGS) -ffreestanding -static -nostdlib -lgcc -nodefaultlibs -Og -g $(LIBC_INCLUDES) -Isrc/include $(LIBC_LIB) -lgcc
 
 define DEFINE_UTIL_RULE
-$(UTILS_ROOT)/build/$(1): $$(shell find $(UTILS_ROOT)/$(1)/src/ -type f -name "*.[cs]" 2>/dev/null) $(LIBC_LIB) $(LIBC_CRT0) $(LIBC_CRTI) $(LIBC_CRTN)
+$(UTILS_BUILD_DIR)/$(1): $$(shell find $(UTILS_ROOT)/$(1)/src/ -type f -name "*.[cs]" 2>/dev/null) $(LIBC_LIB) $(LIBC_CRT0) $(LIBC_CRTI) $(LIBC_CRTN)
 	@$$(PROGRESS_LABEL) Building $$(patsubst $$(MAKE_ROOT)/%,%,$$(abspath $$@))
 	@mkdir -p $$(dir $$@)
 	@files=$$$$(find $(UTILS_ROOT)/$(1)/src/ -type f -name "*.[cs]" 2>/dev/null); \
@@ -26,7 +32,7 @@ endef
 $(foreach util,$(UTILS),$(eval $(call DEFINE_UTIL_RULE,$(util))))
 
 clean::
-	@rm -rf $(UTILS_ROOT)/build
+	@rm -rf $(UTILS_BUILD_DIR)
 
 .PHONY: clean
 
